888841df079795c40bccc3b43dc59b42edb8dc75
shivani <shivaniw69@gmail.com>
2025-03-28 16:33:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+// const name="shivani"
+// const age=11
+// const salary=35000
+
+
+// const App=()=>{
+//   return(
+//     <p>Welcome to react i am {name} my age is {age*2} my salary {salary*3}</p>
+//   )
+// }
+// export default App;
+
+// const subject=<ul>
+//          <li>PHp</li>
+//          <li>Oracle</li>
+//          <li>java</li>
+//          <li>sql</li>
+// </ul>
+
+// const App=()=>{
+//   return(
+//     <h1>My Subject:{subject}</h1>
+//   )
+// }
+// export default App;
+
+//===========================================one top level  element==================================================== 
+const App=()=>{
+  return(
+    <>
+    <h1>welcome to react</h1>
+    <h1> we learn java</h1>
+    <p>i am developer</p>
+    </>
+  )
+}
+export default App;
