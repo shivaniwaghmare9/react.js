6f52142b79c91e0f373f9bd214215a4ee89c6fb7
shivani <shivaniw69@gmail.com>
2025-08-31 23:14:26 +0530

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,6 +2,18 @@
 
 
 
+// const Footer=()=>{
+//     return(
+//       <>
+//      <h1> This is footer</h1>
+//      </>
+//      )
+//    }
+// export default Footer;
+
+
+
+
 const Footer=()=>{
     return(
       <>
