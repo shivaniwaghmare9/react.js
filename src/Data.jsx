002cc4add58d4b7261259d869c8a92e6450c60fe
shivani <shivaniw69@gmail.com>
2025-08-31 23:14:02 +0530

    data

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,18 +2,6 @@
 
 
 
-// const Data=()=>{
-//     return(
-//       <>
-//      <h1 id="myid"> Data site</h1>
-//      </>
-//      )
-//    }
-// export default Data;
-
-
-
-
 const Data=()=>{
     return(
       <>
