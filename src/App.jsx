0c71535154c43e0e9a00c175e6949b97c33357bd
shivani <shivaniw69@gmail.com>
2025-08-31 23:11:37 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -131,151 +131,177 @@
 
 //========================================03-04-2025==========================================================================================
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
 
-import Carousel from 'react-bootstrap/Carousel';
-import img1 from "./images/download.jpg";
-import img2 from "./images/download (1).jpg";
-import img3 from "./images/download (2).jpg";
+// import Carousel from 'react-bootstrap/Carousel';
+// import img1 from "./images/download.jpg";
+// import img2 from "./images/download (1).jpg";
+// import img3 from "./images/download (2).jpg";
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
 
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 
 
 
+// const App=()=>{
+//    return(
+//       <>
+//       <h1>Welcome To React!!!</h1>
+//       <hr />
+//       <Navbar bg="primary" data-bs-theme="dark">
+//         <Container>
+//           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+//           <Nav className="me-auto">
+//             <Nav.Link href="#home">Home</Nav.Link>
+//             <Nav.Link href="#features">About</Nav.Link>
+//             <Nav.Link href="#pricing">Contact</Nav.Link>
+//           </Nav>
+//         </Container>
+//       </Navbar>
+//       <Carousel>
+//       <Carousel.Item>
+//          <img src={img1} width="100%" height="400px"/>
+//         <Carousel.Caption>
+//           <h3>First slide label</h3>
+//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//       <img src={img2}  width="100%" height="400px"/>
+//         <Carousel.Caption>
+//           <h3>Second slide label</h3>
+//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//       <img src={img3}  width="100%" height="400px"/> 
+//         <Carousel.Caption>
+//           <h3>Third slide label</h3>
+//           <p>
+//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//           </p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
+
+
+//     <div id="mycard">
+
+//     <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src={img1} />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+
+//     <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src={img2} />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+      
+//     <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src={img3} />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+      
+//     </div>
+
+//     <Container>
+//       <Row>
+//         <Col>
+//         <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src={img1} height="230"/>
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card></Col>
+
+//     <Col>
+//         <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src={img2} height="230"/>
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//     </Col>
+
+//     <Col>
+//         <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src={img3} height="230"/>
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card></Col>
+//       </Row>
+//     </Container>
+      
+      
+//       </>
+//    )
+// }
+// export default App;
+
+//===============================================05-04-2025()================================================================================================
+// const App=()=>{
+//   return(
+//     <>
+//       <h1>Welcome To React Classes!!!!</h1>
+//       Enter your Name:<input type="text" /><br></br><br></br>
+//       Enter your Number:<input type="text" /><br></br><br></br>
+//       <button>Submit</button>
+//     </>
+//   )
+// }
+// export default App;
 const App=()=>{
-   return(
-      <>
-      <h1>Welcome To React!!!</h1>
-      <hr />
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">About</Nav.Link>
-            <Nav.Link href="#pricing">Contact</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <Carousel>
-      <Carousel.Item>
-         <img src={img1} width="100%" height="400px"/>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={img2}  width="100%" height="400px"/>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={img3}  width="100%" height="400px"/> 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
-
-    <div id="mycard">
-
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img1} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img2} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-      
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img3} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-      
-    </div>
-
-    <Container>
-      <Row>
-        <Col>
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img1} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card></Col>
-
-    <Col>
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img2} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </Col>
-
-    <Col>
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img3} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card></Col>
-      </Row>
-    </Container>
-      
-      
-      </>
-   )
+  return(
+    <>
+      <h1>Welcome To React Classes!!!!</h1>
+      Enter your Name:<input type="text" /><br></br><br></br>
+      Enter your Number:<input type="text" /><br></br><br></br>
+      <button>Submit</button>
+    </>
+  )
 }
 export default App;
+
+
